8c4edbf03bfe4bdcbe1d36d22115b2fd41ea4dee
zivww <ziv.wang.tw@gmail.com>
2020-04-16 23:54:43 +0800

    [Cpp17] 04162020

---
+++ Competitions/LeetCode/30_day_leetcoding_challenge/678_valid_parenthesis_string_04162020.cpp
@@ -0,0 +1,71 @@
+/*******************************************************************
+* https://leetcode.com/problems/valid-parenthesis-string/
+* Medium
+* 
+* Conception: 
+*  1. 
+*
+*  Given a string containing only three types of characters: '(', ')' and '*',
+*  write a function to check whether this string is valid. We define the validity of a string by these rules:
+*
+*    Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+*    Any right parenthesis ')' must have a corresponding left parenthesis '('.
+*    Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+*    '*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string.
+*    An empty string is also valid.
+*
+*
+* Example:
+*
+* Key:
+*  1. 
+*
+* Advanced:
+*  1. 
+*
+*******************************************************************/
+class Solution {
+public:
+    bool checkValidString(string s) {
+        if(s.empty()) return true;
+
+        stack<char> star;
+        stack<char> cache;
+
+        int pos = 0;
+        for(auto c : s){
+            if(c == '('){
+                cache.push(pos);
+            }else if(c == ')'){
+                if(!cache.size() && !star.size()){
+                    return false;
+                }
+
+                if(cache.size()){
+                    cache.pop();
+                }else{
+                    star.pop();
+                }
+            }else{
+                star.push(pos);
+            }
+            pos++;
+        }
+        
+        if(cache.size() > star.size()){
+            return false;
+        }
+        while(cache.size() && star.size()){
+            if(star.top()<cache.top()){
+                return false;
+            }
+            cache.pop();
+            star.pop();
+        }
+        
+
+        
+        return true;
+        
+    }
+};
