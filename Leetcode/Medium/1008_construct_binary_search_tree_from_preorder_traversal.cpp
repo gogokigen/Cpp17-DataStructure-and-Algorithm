576a3f36b501cca26ce68d4fd1687722d2f9a82a
zivww <ziv.wang.tw@gmail.com>
2020-04-20 00:51:27 +0800

    [Cpp17] _binary_search_tree_from_preorder_traversal

---
+++ Leetcode/Medium/1008_construct_binary_search_tree_from_preorder_traversal.cpp
@@ -0,0 +1,58 @@
+/*******************************************************************
+* https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
+* Medium
+* 
+* Conception: 
+*  1. preorder
+*
+* Return the root node of a binary search tree that matches the given preorder traversal.
+*
+* Example:
+*
+* Key:
+*  1.
+*
+* Reference:
+*  1. https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/discuss/252273/C%2B%2B-O(n-log-n)-and-O(n)
+*
+*******************************************************************/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        if (preorder.size() == 0) 
+            return NULL;
+
+        TreeNode* root = new TreeNode(preorder[0]);
+        stack<TreeNode*> s;
+        s.push(root);
+
+        for(int i = 1; i < preorder.size(); i++){
+            if(s.top()->val > preorder[i]){
+                TreeNode * temp = new TreeNode(preorder[i]);
+                s.top()->left = temp;
+                s.push(temp); 
+            }else {
+                TreeNode* child = NULL;
+                while(!s.empty()
+                      &&s.top()->val < preorder[i]){
+                    child = s.top();
+                    s.pop();
+                }
+                TreeNode * newNode = new TreeNode(preorder[i]);
+                child->right = newNode;
+                s.push(newNode);
+            }
+        }
+        return root;
+    }
+};
