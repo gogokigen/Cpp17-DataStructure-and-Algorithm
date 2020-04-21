f32e0395ba5328455e5282b0aabd2d75444e10e8
zivww <ziv.wang.tw@gmail.com>
2020-04-22 00:17:59 +0800

    [Cpp17] quick sort

---
+++ Interview/AlgorithmPractice/quick_sort.cpp
@@ -0,0 +1,65 @@
+/*******************************************************************
+* 
+* Practice: Quick Sort
+* 
+* References: 
+*  1. https://www.youtube.com/watch?v=PgBzjlCcFvc
+*  2. https://www.geeksforgeeks.org/quick-sort/
+*
+* Key:
+*  1. 
+*
+*******************************************************************/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    Solution(){};
+    void swap(int* a, int* b)  {  
+        int t = *a;  
+        *a = *b;  
+        *b = t;  
+    }  
+    int helper(vector<int>&list, int start, int end ){
+        int pivot = list[end];
+        int i = start-1; //index of smaller number
+        for(int j = start; j < end; j++){
+            if(list[j] < pivot){
+                i++;
+                swap(&list[i], &list[j]);
+            }
+        }
+        swap(&list[i+1], &list[end]);
+        return i + 1;
+    }
+
+    void quickSort(vector<int> &list, int start, int end ){
+        if(start < end){
+            int partition = helper(list, start, end);
+
+            quickSort(list, start, partition - 1);
+            quickSort(list, partition+1, end);
+        }
+    }
+
+};
+
+int main(){
+    //Test Cases
+    vector<int> list = { 10, 9, 8, 2 , 4 , 6, 7};
+
+
+    Solution* sol = new Solution();
+
+    sol->quickSort(list, 0, (list.size()-1));
+
+    for(auto& i:list){
+        cout << i << ", ";
+    }
+
+    return 0;
+}
