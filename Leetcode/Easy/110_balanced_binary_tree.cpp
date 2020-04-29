07857c2630d3018d878ad1f6048b841db7297d36
zivww <ziv.wang.tw@gmail.com>
2020-04-30 00:43:00 +0800

    [Cpp17] trees v3

--- Leetcode/Easy/110_balanced_binary_tree.cpp
+++ Leetcode/Easy/110_balanced_binary_tree.cpp
@@ -20,19 +20,33 @@
 *  1. 
 *
 *******************************************************************/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
+private:
+    int maxDeep(TreeNode* root){
+        if(root == NULL) return -1;
+        
+        int L = maxDeep(root->left) + 1;
+        int R = maxDeep(root->right) + 1;
+        return max(L, R);
+    }
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        if(nums.size()==0||nums.size()==1) return false;
-        set<int> cache;
-
-        for(int i = 0; i < nums.size(); i++){
-            if(cache.find(nums[i]) != cache.end()){//exist
-                return true;
-            }else{
-                cache.insert(nums[i]);
-            }
-        }
-        return false;
+    bool isBalanced(TreeNode* root) {
+        if(root == NULL) return true;
+        
+        int deepL = maxDeep(root->left);
+        int deepR = maxDeep(root->right);
+        
+        return abs(deepL - deepR) < 2 && isBalanced(root->left) && isBalanced(root->right) ;
     }
 };
