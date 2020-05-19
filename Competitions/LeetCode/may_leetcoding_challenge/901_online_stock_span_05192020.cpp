d91306663d426d6e4bd0d10ee9701d1aaa7a7d64
zivww <ziv.wang.tw@gmail.com>
2020-05-19 20:58:00 +0800

    [Cpp17] 05193020

---
+++ Competitions/LeetCode/may_leetcoding_challenge/901_online_stock_span_05192020.cpp
@@ -0,0 +1,52 @@
+/*******************************************************************
+* https://leetcode.com/problems/online-stock-span/
+* Medium
+* 
+* Conception: 
+*  1. 
+*
+* Write a class StockSpanner which collects daily price quotes for some stock,
+* and returns the span of that stock's price for the current day.
+*
+* The span of the stock's price today is
+* defined as the maximum number of consecutive days (starting from today and going backwards)
+* for which the price of the stock was less than or equal to today's price.
+*
+*
+* Key:
+*  1. 
+*
+* Explanation:
+*
+*******************************************************************/
+class StockSpanner {
+private:
+    vector<int> dp;
+    vector<int> prices;
+    int pos = 0;
+public:
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        if( pos == 0 || price < prices.back()){
+            dp.push_back(1);
+        }else{
+            int j = pos - 1;
+            while(j >= 0 && price >= prices[j]){
+                j -= dp[j];
+            }
+            dp.push_back(pos - j);
+        }
+        pos++;
+        prices.push_back(price);
+        return dp.back();
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
