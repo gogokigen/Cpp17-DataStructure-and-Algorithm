5549f4afaaa23d747cc06e5d80e9ae3a56c278c4
zivww <ziv.wang.tw@gmail.com>
2020-07-07 01:06:11 +0800

    [Cpp17] remove duplicate char

---
+++ Leetcode/Hard/316_remove_duplicate_letters.cpp
@@ -0,0 +1,49 @@
+/*******************************************************************
+* https://leetcode.com/problems/remove-duplicate-letters/
+* Hard
+* 
+* Conception: 
+*  1.
+*
+* Return the lexicographically smallest subsequence of text that
+* contains all the distinct characters of text exactly once.
+*
+*
+* Example:
+* 
+*
+* Key:
+*  1.
+*
+* References:
+*  1.
+*
+*******************************************************************/
+class Solution {
+public:
+    string smallestSubsequence(string text) {
+        int n = text.length();
+        
+        unordered_map<char, int> table;
+        for (int i = 0; i < n; i++)
+            table[text[i]]++;
+        
+        unordered_map<char, bool>seen;
+        string res;
+        
+        for (int i = 0; i < n; i++) {
+            table[text[i]]--;
+            if (seen[text[i]]) continue;
+            seen[text[i]] = true;
+            
+            while(!res.empty() && res.back() > text[i] && table[res.back()] > 0) {
+                seen[res.back()] = false;
+                res.pop_back();
+            }
+            
+            res.push_back(text[i]);
+        }
+        
+        return res;
+    }
+};
