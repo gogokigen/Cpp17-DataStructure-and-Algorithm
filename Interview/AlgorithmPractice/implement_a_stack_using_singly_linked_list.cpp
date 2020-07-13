1dc3a39bc1f576697e016d7c3dbb39548751b3eb
zivww <ziv.wang.tw@gmail.com>
2020-07-14 00:31:42 +0800

    [Cpp17] stack

--- Interview/AlgorithmPractice/implement_a_stack_using_singly_linked_list.cpp
+++ Interview/AlgorithmPractice/implement_a_stack_using_singly_linked_list.cpp
@@ -7,104 +7,103 @@ using namespace std;
   
 struct Node { 
     int data; 
-    struct Node* next; 
+    Node* next; 
 };
 
 
 class Stack{
-public:
+private:
+    Node* top;
+    int capacity = 0;
 
-struct Node* top;
-int capacity = 0;
+public:
     
     Stack(int max_size):top(NULL), capacity(max_size){}
 
-// Utility function to add an element data in the stack 
- // insert at the beginning 
-void push(int data) { 
-    // create new node temp and allocate memory 
-    struct Node* temp; 
-    temp = new Node(); 
-  
-    // check if stack (heap) is full. Then inserting an element would 
-    // lead to stack overflow 
-    if (!temp) { 
-        cout << "\nHeap Overflow" << endl; 
-        exit(1); 
+    // Utility function to add an element data in the stack 
+     // insert at the beginning 
+    void push(int data) { 
+        // create new node temp and allocate memory 
+        Node* temp; 
+        temp = new Node(); 
+      
+        // check if stack (heap) is full. Then inserting an element would 
+        // lead to stack overflow 
+        if (!temp) { 
+            cout << "\nHeap Overflow" << endl; 
+            exit(1); 
+        } 
+      
+        // initialize data into temp data field 
+        temp->data = data; 
+      
+        // put top pointer reference into temp link 
+        temp->next = this->top; 
+      
+        // make temp as top of Stack 
+        this->top = temp; 
     } 
   
-    // initialize data into temp data field 
-    temp->data = data; 
-  
-    // put top pointer reference into temp link 
-    temp->next = this->top; 
-  
-    // make temp as top of Stack 
-    this->top = temp; 
-} 
-  
-// Utility function to check if the stack is empty or not 
-int isEmpty() { 
-    return this->top == NULL; 
-} 
-  
-// Utility function to return top element in a stack 
-int peek() { 
-    // check for empty stack 
-    if (!isEmpty()) 
-        return this->top->data; 
-    else
-        exit(1); 
-} 
-  
-// Utility function to pop top  
-// element from the stack 
-  
-void pop() { 
-    struct Node* temp; 
-  
-    // check for stack underflow 
-    if (this->top == NULL) { 
-        cout << "\nStack Underflow" << endl; 
-        exit(1); 
+    // Utility function to check if the stack is empty or not 
+    int isEmpty() { 
+        return this->top == NULL; 
     } 
-    else { 
-        // top assign into temp 
-        temp = this->top; 
-  
-        // assign second node to top 
-        this->top = this->top->next; 
-  
-        // destroy connection between first and second 
-        temp->next = NULL; 
-  
-        // release memory of top node 
-        free(temp); 
+
+    // Utility function to return top element in a stack 
+    int peek() { 
+        // check for empty stack 
+        if (!isEmpty()) 
+            return this->top->data; 
+        else
+            exit(1); 
     } 
-} 
   
-// Function to print all the  
-// elements of the stack  
-void display()  { 
-    struct Node* temp; 
-  
-    // check for stack underflow 
-    if (this->top == NULL) { 
-        cout << "\nStack Underflow"; 
-        exit(1); 
-    } 
-    else { 
-        temp = this->top; 
-        while (temp != NULL) { 
-  
-            // print node data 
-            cout <<  temp->data << " " << endl; 
-  
-            // assign temp link to temp 
-            temp = temp->next; 
+    // Utility function to pop top  
+    // element from the stack 
+    void pop() { 
+        Node* temp; 
+      
+        // check for stack underflow 
+        if (this->top == NULL) { 
+            cout << "\nStack Underflow" << endl; 
+            exit(1); 
+        } 
+        else { 
+            // top assign into temp 
+            temp = this->top; 
+      
+            // assign second node to top 
+            this->top = this->top->next; 
+      
+            // destroy connection between first and second 
+            temp->next = NULL; 
+      
+            // release memory of top node 
+            free(temp); 
         } 
     } 
-} 
+  
+    // Function to print all the  
+    // elements of the stack  
+    void display()  { 
+        Node* temp; 
+      
+        // check for stack underflow 
+        if (this->top == NULL) { 
+            cout << "\nStack Underflow"; 
+            exit(1); 
+        } else { 
+            temp = this->top; 
+            while (temp != NULL) { 
+      
+                // print node data 
+                cout <<  temp->data << " " << endl; 
+      
+                // assign temp link to temp 
+                temp = temp->next; 
+            } 
+        } 
+    } 
 
 };
 
