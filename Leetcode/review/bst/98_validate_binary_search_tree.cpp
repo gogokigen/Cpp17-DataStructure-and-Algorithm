4a18a47e4b40c1d992823a0a2061d72cb3a29a42
zivww <ziv.wang.tw@gmail.com>
2020-11-24 00:35:45 +0800

    [Cpp17] review bst

--- Leetcode/review/bst/98_validate_binary_search_tree.cpp
+++ Leetcode/review/bst/98_validate_binary_search_tree.cpp
@@ -78,3 +78,26 @@ public:
         return inorderTraverse(root, min);
     }
 };
+
+// Iteration
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        long pre = LONG_MIN;
+        stack<TreeNode*> todo;
+        while (root || !todo.empty()) {
+            while (root) {
+                todo.push(root);
+                root = root -> left;
+            }
+            root = todo.top();
+            todo.pop();
+            if (root -> val <= pre) {
+                return false;
+            }
+            pre = root -> val;
+            root = root -> right;
+        }
+        return true;
+    }
+};
