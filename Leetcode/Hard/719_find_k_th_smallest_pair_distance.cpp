3942639f4610640b8677a2c81095732f3871ba16
zivww <ziv.wang.tw@gmail.com>
2020-12-07 19:04:30 +0800

    [Cpp17] review k-element 215

--- Leetcode/Hard/719_find_k_th_smallest_pair_distance.cpp
+++ Leetcode/Hard/719_find_k_th_smallest_pair_distance.cpp
@@ -45,3 +45,37 @@ public:
         return lo;
     }
 };
+
+
+//Heap, but TLE
+class Solution {
+public:
+    int smallestDistancePair(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());//O(nlgn)
+        int len = nums.size();
+        priority_queue<pip,vector<pip>,comp> minHeap;
+        for(int i = 0; i < len-1; i++){
+            int diff = abs(nums[i] - nums[i+1]);
+            minHeap.push({diff, i, i+1});
+        }
+        while(--k && !minHeap.empty()){
+            auto u = minHeap.top();
+            minHeap.pop();
+            int x = u[1];
+            int y = u[2];
+            if(y < len-1){
+                int newDiff = abs(nums[x] - nums[y+1]);
+                minHeap.push({newDiff, x, y+1});
+            }
+        }
+        return minHeap.top()[0];
+    }
+private:
+    //1st-diff, 2nd-index of start ele, 3rd-index of prob ele
+    using pip = vector<int>;
+    struct comp{
+        bool operator()(const pip& a, const pip& b){
+            return a[0] > b[0];
+        }
+    };
+};
