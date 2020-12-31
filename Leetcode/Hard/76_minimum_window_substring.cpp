a0391b5cb3e5196c307278592c09401cac7e8a23
zhuziicafe <zanna.wang.tw@gmail.com>
2020-12-31 11:02:14 +0800

    [Cpp17] array and string 76

---
+++ Leetcode/Hard/76_minimum_window_substring.cpp
@@ -0,0 +1,61 @@
+/*******************************************************************
+* https://leetcode.com/problems/minimum-window-substring/
+* Hard
+* 
+* Conception: 
+*  1. 
+*
+* Given two strings s and t,
+* return the minimum window in s which will contain all the characters in t.
+* If there is no such window in s that covers all characters in t,
+* return the empty string "".
+*
+*
+* Example:
+*
+* Key:
+*  1. 
+*
+* References:
+*  1.
+*
+*******************************************************************/
+class Solution {
+public:
+    void fillFreq(int a[], string str){
+        for(char c : str)
+            a[c-'A']++;
+    }
+    
+    string minWindow(string s, string t) {
+
+        int freq[256]={0}, window_freq[256]={0};
+        int start = 0, n = s.size();
+        int min_wind = INT_MAX, i, j, ctr = 0;
+        fillFreq(freq,t);
+        
+        for(int end = 0; end < n; end++){
+            int ind =  s[end]-'A';
+            
+            // check if char exist in t and not all of it's instances are already in window...
+            if(freq[ind] && window_freq[ind] < freq[ind]){ 
+                ctr++;
+            }
+            window_freq[ind]++; // add char in window
+            
+            if(ctr == t.size()){ // window found with all char's of t
+                // try to reduce current window so that all characters of t are there
+                while( start < end && (window_freq[s[start]-'A'] > freq[s[start]-'A'] || freq[s[start]-'A']==0)){
+                    window_freq[s[start]-'A']--;
+                    start++;
+                }
+                
+                if(min_wind > end - start + 1){
+                    min_wind = end - start + 1;
+                    i = start;
+                }
+            }
+        }
+        return min_wind==INT_MAX ? "" : s.substr(i,min_wind);
+    }
+};
